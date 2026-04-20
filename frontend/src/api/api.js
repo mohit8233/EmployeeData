640cd6855bfd4ac301e69512233b976c6c4c737d
Mohit Singh <mohitbanna085@gmail.com>
2026-04-20 18:04:24 +0530

    renderdeploy

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000/api/employees"
+const BASE_URL = "https://employeedata-3.onrender.com/api/employees"
 // const BASE_URL = "https://employeedata-89u4.onrender.com/api/employees"
 
 
