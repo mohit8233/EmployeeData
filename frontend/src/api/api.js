a1eb3b3dd5747c1c02ca8a107ad4f3c17eddb2f8
Mohit Singh <mohitbanna085@gmail.com>
2026-04-27 12:34:34 +0530

    UpdateFrontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "https://employeedata-2.onrender.com/api/employees";
+const BASE_URL = "https://employeedata-w0fo.onrender.com/api/employees";
 
 
-const AUTH_URL = "https://employeedata-2.onrender.com/api/authRoutes";
+const AUTH_URL = "https://employeedata-w0fo.onrender.com/api/authRoutes";
 
 
 // ================= EMPLOYEE =================
